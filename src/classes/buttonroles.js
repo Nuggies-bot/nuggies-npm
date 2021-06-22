03c5c4586c33b766222f01fc5aec843cec566104
Assassin#1234 <chordsandfrets@gmail.com>
2021-06-22 18:47:20 +0530

    finalized buttonroles

--- src/classes/buttonroles.js
+++ src/classes/buttonroles.js
@@ -1,5 +1,5 @@
 const { MessageButton, MessageActionRow } = require('discord-buttons');
-const { MessageEmbed, Message } = require('discord.js');
+const { MessageEmbed, Message, Channel } = require('discord.js');
 class buttonroles {
 
 	constructor() {
@@ -24,12 +24,23 @@ class buttonroles {
 	}
 	toJSON() { return { roles: this.roles }; }
 
-	static async create({ message, embed, role }) {
+	/**
+	 * @param {Message} message - The Discord Message
+	 * @param {MessageEmbed} embed - The Discord Embed
+	 * @param {buttonroles} role - The created object using .buttonroles().addrole()
+	 * @param {String} channelID - the id of the channel you want to send the message to.
+	 */
+	static async create({ message, content, role, channelID }) {
+		if((message instanceof Message) == false) throw new TypeError('please provide the Discord Message');
+		if(!content) throw new Error('please provide content!');
+		if(!role) throw new Error('role not provided!');
+		if(!channelID) throw new Error('channelID not provided!');
 		const buttons = [];
 		const rows = [];
 		// Promise.resolve(role).then(console.log);
+		// console.log(role);
 		for (const buttonObject of role.roles) {
-			buttons.push(new MessageButton().setStyle(buttonObject.color).setEmoji(buttonObject.emoji).setLabel(buttonObject.label).setID(`br-${buttonObject.role}`));
+			buttons.push(new MessageButton().setStyle(buttonObject.color).setEmoji(buttonObject.emoji).setLabel(buttonObject.label).setID(`br:${buttonObject.role}`));
 		}
 		for (let i = 0; i < Math.ceil(role.roles.length / 5); i++) {
 			rows.push(new MessageActionRow());
@@ -37,7 +48,28 @@ class buttonroles {
 		rows.forEach((row, i) => {
 			row.addComponents(buttons.slice(0 + (i * 5), 5 + (i * 5)));
 		});
-		message.channel.send({ embed: embed, components: rows });
+		content instanceof MessageEmbed ? message.client.channels.cache.get(channelID).send({ embed: content, components: rows }) : message.client.channels.cache.get(channelID).send(content, { components: rows });
+	}
+	static async buttonclick(client, button) {
+		if(!client) throw new Error('client not provided in buttonclick!');
+		if(!button) throw new Error('Button not provided!');
+		await button.clicker.fetch();
+		const id = button.id;
+		if (id.startsWith('br')) {
+			let member;
+			const fetchMem = await button.guild.members.fetch(button.clicker.member.id, false);
+			if (fetchMem) member = button.guild.members.cache.get(button.clicker.member.id);
+			await member.fetch(true);
+			const role = id.split(':')[1];
+			if (button.clicker.member.roles.cache.has(role)) {
+				button.clicker.member.roles.remove(role);
+				button.reply.send(`I have removed the <@&${role}> role from you!`, true);
+			}
+			else {
+				button.clicker.member.roles.add(role);
+				button.reply.send(`I have added the <@&${role}> role to you!`, true);
+			}
+		}
 	}
 }
 
