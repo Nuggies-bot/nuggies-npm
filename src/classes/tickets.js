6b6c3f68a6361db54d32a877abf551080a0738e4
Assassin#1234 <chordsandfrets@gmail.com>
2021-06-29 12:19:20 +0530

    setParentID function

--- src/classes/tickets.js
+++ src/classes/tickets.js
@@ -1,5 +1,11 @@
+const schema = require('../models/ticketsschema');
 class tickets {
-
+	async setParentID(categoryID, guildID) {
+		const data = await schema.findOne({ guildID: guildID });
+		data.parentID = categoryID;
+		await data.save();
+		return data;
+	}
 }
 
 module.exports = tickets;
