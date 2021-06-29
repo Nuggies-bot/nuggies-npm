51468d22de5015637ad4537f4bb40948e42f8a5c
Assassin#1234 <chordsandfrets@gmail.com>
2021-06-29 18:11:02 +0530

    export

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 module.exports.giveaways = require('./src/classes/giveaways');
 module.exports.buttonroles = require('./src/classes/buttonroles');
+module.exports.tickets = require('./src/classes/tickets');
