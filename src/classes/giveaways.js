e966f49e34232189d9584dc67c850fc11ff8bb0d
Assassin#1234 <chordsandfrets@gmail.com>
2021-06-30 20:21:26 +0530

    removing connect and buttonclick from giveaways

--- src/classes/giveaways.js
+++ src/classes/giveaways.js
@@ -5,24 +5,9 @@ const Discord = require('discord.js');
 const mongoose = require('mongoose');
 const ms = require('ms');
 mongoose.set('useFindAndModify', false);
-let connection;
 let win;
 
 class giveaways {
-	/**
-	*
-	* @param {string} url - MongoDB connection URI.
-	*/
-	static async connect(url) {
-		if (!url) throw new TypeError('NuggiesError: You didn\'t provide a MongoDB connection string');
-
-		connection = url;
-
-		return mongoose.connect(url, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-	}
 	/**
 	 * @param {any} - The Discord Message
 	 * @param {String} - the giveaway prize
@@ -110,97 +95,6 @@ class giveaways {
 		const link = `https://discord.com/channels/${data.guildID}/${data.channelID}/${data.messageID}`;
 		const button = new MessageButton().setLabel('Giveaway').setStyle('url').setURL(link);
 		return button;
-	}
-	static async buttonclick(client, button) {
-		if (!client) throw new Error('NuggiesError: client not provided');
-		if (!button) throw new Error('NuggiesError: button not provided');
-		await button.clicker.fetch();
-		const id = button.id;
-		if (id.startsWith('giveaways')) {
-			const tag = id.split('-');
-			// if(tag[1] === 'drop') {
-			// 	console.log('works till here');
-			// 	button.channel.send(`<@${button.clicker.user.id}> won the drop for **${tag.slice(2).join(' ')}**! congratulations!`);
-			// 	await utils.editDropButtons(client, button, tag.slice(2).join(' '));
-			// }
-			if (tag[1] === 'enter') {
-				const data = await schema.findOne({ messageID: button.message.id });
-				if (data.requirements.enabled == true) {
-					if (data.clickers.includes(button.clicker.user.id)) { return button.reply.send('you already entered this giveaway!', true); }
-					const roles = data.requirements.roles.map(x => button.message.guild.members.cache.get(button.clicker.user.id).roles.cache.get(x));
-					if (!roles[0]) {
-						return button.reply.send(`You do not have the required role(s)\n${button.message.guild.roles.cache.filter(x => data.requirements.roles.includes(x.id)).filter(x => !button.message.guild.members.cache.get(button.clicker.user.id).roles.cache.get(x.id)).array().map(x => `\`${x.name}\``).join(', ')}\n for the giveaway!`, true);
-					}
-				}
-				if (!data.clickers.includes(button.clicker.user.id)) {
-					data.clickers.push(button.clicker.user.id);
-					data.save();
-					return button.reply.send('You have entered this giveaway! best of luck :)', true);
-				}
-			}
-			if (tag[1] === 'reroll') {
-				if (button.clicker.user.id !== tag[2]) return button.reply.send('You cannot end this giveaway as you didnt host it!', { ephemeral: true });
-				try {
-					button.defer();
-					win = await this.reroll(client, button.message.id);
-				}
-				catch (err) {
-					console.log(err);
-					return button.message.channel.send('unable to find the giveaway!');
-				}
-				if (!win.length) return button.message.channel.send('There are not enough people in the giveaway!');
-				button.message.channel.send(`Rerolled! <@${win}> is the new winner of the giveaway!`, { component: new MessageButton().setLabel('Giveaway').setURL(`https://discord.com/channels/${button.message.guild.id}/${button.message.channel.id}/${button.message.id}`).setStyle('url') });
-			}
-			if (tag[1] === 'end') {
-				if (button.clicker.user.id !== tag[2]) return button.reply.send('You cannot end this giveaway as you didnt host it!', { ephemeral: true });
-				await this.endByButton(client, button.message.id, button);
-			}
-		}
-	}
-	static async endByButton(client, messageID, button) {
-		if (!client) throw new Error('NuggiesError: client not provided in button end');
-		if (!messageID) throw new Error('NuggiesError:  ID not provided in button end');
-		if (!button) throw new Error('NuggiesError: button not provided in button end');
-		await button.defer();
-		const data = await this.getByMessageID(messageID);
-		const msg = await client.guilds.cache.get(data.guildID).channels.cache.get(data.channelID).messages.fetch(messageID);
-		const res = (await this.end(msg, data, msg));
-		if (res == 'ENDED') button.reply.send('The giveaway has already ended!', { ephemeral: true });
-	}
-
-	static async end(message, data, giveawaymsg) {
-		if (!message) throw new Error('NuggiesError: message wasnt provided in end');
-		if (!data) throw new Error('NuggiesError: data wasnt provided in end');
-		if (!giveawaymsg) throw new Error('NuggiesError: giveawaymsg wasnt provided in end');
-		if ((await this.getByMessageID(data.messageID)).ended) return 'ENDED';
-		const winners = await utils.choose(data.winners, message.id);
-
-		if (!winners) {
-			message.channel.send('Not enough people participated in this giveaway.');
-			data.ended = true;
-			data.save();
-			const embed = giveawaymsg.embeds[0];
-			embed.description = `🎁 Prize: **${data.prize}**\n🎊 Hosted by: <@${data.host.toString()}>\n⏲️ Winner(s): none`;
-			giveawaymsg.edit('', { embed: embed });
-			utils.editButtons(message.client, data);
-			return 'NO_WINNERS';
-		}
-
-		message.channel.send(`${winners.map(winner => `<@${winner}>`).join(', ')} you won ${data.prize} Congratulations! Hosted by ${message.guild.members.cache.get(data.host).toString()}`, { component: await this.gotoGiveaway(data) });
-		const dmEmbed = new Discord.MessageEmbed()
-			.setTitle('You won!')
-			.setDescription(`You have won a giveaway in **${giveawaymsg.guild.name}**!\nPrize: [${data.prize}](https://discord.com/${giveawaymsg.guild.id}/${giveawaymsg.channel.id}/${data.messageID})`)
-			.setColor('RANDOM')
-			.setFooter('GG!');
-		winners.forEach((user) => {
-			message.guild.members.cache.get(user).send(dmEmbed);
-		});
-		const embed = giveawaymsg.embeds[0];
-		embed.description = `🎁 Prize: **${data.prize}**\n🎊 Hosted by: <@${data.host.toString()}>\n⏲️ Winner(s): ${winners.map(winner => `<@${winner}>`).join(', ')}`;
-		giveawaymsg.edit('', { embed: embed });
-		data.ended = true;
-		data.save();
-		utils.editButtons(message.client, data);
 	}
 	static async reroll(client, messageID) {
 		if (!client) throw new Error('NuggiesError: client wasnt provided in reroll');
