5c1aa8d9c744f4f091799b2e5a711fa5c175d5e4
Assassin#1234 <chordsandfrets@gmail.com>
2021-07-10 15:54:26 +0530

    deleteapplication

--- src/classes/applications.js
+++ src/classes/applications.js
@@ -1,4 +1,6 @@
-const schema = require('../models/applicationsschema');
+const { MessageActionRow, MessageMenu, MessageMenuOption } = require('discord-buttons');
+const schema = require('../models/applictionsschema');
+
 class applications {
 	/**
      *
@@ -18,7 +20,7 @@ class applications {
 		if(!description) throw new Error('NuggiesError: description not provided');
 		if(!label) throw new Error('NuggiesError: label not provided');
 		if(typeof guildID !== 'string') throw new Error('NuggiesError: guildID must be a string');
-		if(typeof questions !== Array) throw new Error('NuggiesError: questions must be an array');
+		// if(!questions.isArray) throw new Error('NuggiesError: questions must be an array');
 		if(typeof name !== 'string') throw new Error('NuggiesError: name must be a string');
 		if(typeof emoji !== 'string') throw new Error('NuggiesError: emoji must be a string');
 		if(typeof channel !== 'string') throw new Error('NuggiesError: channel must be a string');
@@ -44,6 +46,23 @@ class applications {
 		}
 		data.save();
 		return data;
+	}
+	static async deleteapplication({ guildID, name }) {
+		const data = await schema.findOne({ guildID: guildID });
+		if(!data) return false;
+		if(data) {
+			const index = await data.applications.findIndex(function(array, index) {
+				return array.name === name;
+			});
+			data.applications.splice(index, 1);
+			data.save();
+			return true;
+		}
+	}
+	static async getDropdownComponent({ guildID }) {
+		if(!guildID) throw new Error('NuggiesError: GuildID not provided');
+		const data = schema.findOne({ guildID: guildID });
+		if(data.applications);
 	}
 }
 
