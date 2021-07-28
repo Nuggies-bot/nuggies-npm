3098f504a9798bc59b39b9d33d821e03cce03962
SuperSweatyTejas <77419101+SuperSweatyTejas@users.noreply.github.com>
2021-07-28 14:21:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = require('./src/classes/main');
+module.exports.Pagination = require('./src/functions/pagination.js')
 module.exports.giveaways = require('./src/classes/giveaways');
 module.exports.buttonroles = require('./src/classes/buttonroles');
 module.exports.dropdownroles = require('./src/classes/dropdownroles');
