edf9d76efc886518c36ed5e43cc58248567cac2c
Assassin#1234 <chordsandfrets@gmail.com>
2021-09-27 13:10:37 +0530

    AmariReq (untested)

--- src/functions/utils.js
+++ src/functions/utils.js
@@ -1,6 +1,6 @@
 const { version } = require('discord.js');
 const { DJS_STYLES, DISBUT_STYLES, toV13 } = require('../constants');
-
+const https = require('https');
 module.exports.convertButtonStyle = (style) => {
 	const isv13 = version.startsWith('13');
 	if (this.isDjsButtonStyle(style) === null) throw new Error('Invalid style provided');
@@ -13,4 +13,28 @@ module.exports.convertButtonStyle = (style) => {
 module.exports.isDjsButtonStyle = (style) => {
 	if (!DJS_STYLES.includes(style) && !DISBUT_STYLES.includes(style)) return null;
 	return DJS_STYLES.includes(style);
+};
+
+module.exports.getAmariData = (key, userID, guildID) => {
+	const options = {
+		hostname: 'amaribot.com',
+		path: `/api/v1/guild/${guildID}/member/${userID}`,
+		auth: key,
+		method: 'GET',
+		port: 8000,
+	};
+	const req = https.request(options, res => {
+		res.on('data', data => {
+			if(!res.statusCode == 200) {return 'ERROR_CODE';}
+			else {
+				return data.toString();
+			}
+		});
+	});
+	req.on('error', error => {
+		console.error(error);
+		return 'ERROR_CODE';
+	});
+
+	req.end();
 };
