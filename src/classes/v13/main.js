ba174f5a3b34d4801aa0a6bdab3ccdb8e4cedf18
Assassin#1234 <chordsandfrets@gmail.com>
2021-09-27 23:36:18 +0530

    feat(giveaways): new default messages

--- src/classes/v13/main.js
+++ src/classes/v13/main.js
@@ -8,21 +8,21 @@ const merge = require('deepmerge');
 
 const defaultGiveawayMessages = {
 	dmWinner: true,
-	giveaway: '🎉🎉 **GIVEAWAY MOMENT** 🎉🎉',
+	giveaway: '🎉🎉 **GIVEAWAY!** 🎉🎉',
 	giveawayDescription: '🎁 Prize: **{prize}**\n🎊 Hosted by: {hostedBy}\n⏲️ Winner(s): `{winners}`\n\nRequirements: {requirements}',
-	endedGiveawayDescription: '🎁 Prize: **{prize}**\n🎊 Hosted by: {hostedBy}\n⏲️ Winner(s): {winners}',
+	endedGiveawayDescription : '🎁 Prize: **{prize}**\n🎊 Hosted by: {hostedBy}\n⏲️ Winner(s): {winners}',
 	giveawayFooterImage: 'https://cdn.discordapp.com/emojis/843076397345144863.png',
-	winMessage: '{winners} you won {prize} Congratulations! Hosted by {hostedBy}',
+	winMessage: 'congrats {winners}! you won `{prize}`!! Total `{totalParticipants}` members participated and your winning percentage was `{winPercentage}%`',
 	rerolledMessage: 'Rerolled! {winner} is the new winner of the giveaway!', // only {winner} placeholder
 	toParticipate: '**Click the Enter button to enter the giveaway!**',
-	newParticipant: 'You have successfully entered for this giveaway', // no placeholders | ephemeral
+	newParticipant: 'You have successfully entered for this giveaway! your win percentage is `{winPercentage}%` among `{totalParticipants}` other participants', // no placeholders | ephemeral
 	alreadyParticipated: 'you already entered this giveaway!', // no placeholders | ephemeral
 	noParticipants: 'There are not enough people in the giveaway!', // no placeholders
 	noRole: 'You do not have the required role(s)\n{requiredRoles}\n for the giveaway!', // only {requiredRoles} | ephemeral
 	dmMessage: 'You have won a giveaway in **{guildName}**!\nPrize: [{prize}]({giveawayURL})',
 	noWinner: 'Not enough people participated in this giveaway.', // no {winner} placerholder
 	alreadyEnded: 'The giveaway has already ended!', // no {winner} placeholder
-	dropWin: '{winner} Won The Drop!!', // only {winner} placeholder
+	dropWin: '{winner} Won The Drop!!',
 };
 const defaultButtonRolesMessages = {
 	addMessage: 'I have added the {role} role to you!',
