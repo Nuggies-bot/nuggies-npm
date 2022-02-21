01e8ad6344b9c1b2a63a56969d8de0211a0c965c
Lightninbolt986 <armaan5740@ggn.hxls.org>
2022-02-21 13:08:53 +0530

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -16,7 +16,7 @@ const defaultGiveawayMessages = {
 	noParticipants: 'There are not enough people in the giveaway!', // no placeholders
 	noRole: 'You do not have the required role(s)\n{requiredRoles}\n for the giveaway!', // only {requiredRoles} | ephemeral
 	dmMessage: 'You have won a giveaway in **{guildName}**!\nPrize: [{prize}]({giveawayURL})',
-	dmMessageHost: 'Your in **{guildName}** has ended!\nPrize: [{prize}]({giveawayURL})',
+	dmMessageHost: 'Your giveaway in **{guildName}** has ended!\nPrize: [{prize}]({giveawayURL})',
 	noWinner: 'Not enough people participated in this giveaway.', // no {winner} placerholder
 	alreadyEnded: 'The giveaway has already ended!', // no {winner} placeholder
 	dropWin: '{winner} Won The Drop!!',
